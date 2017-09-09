78084705bcf03b5ca6ddbaf0ce2596764e131110
knknkn1162 <knknkn1162@gmail.com>
2017-09-09 17:23:44 +0900

    fix args of UserThread ctor

--- src/ch2/UserThread.java
+++ src/ch2/UserThread.java
@@ -7,16 +7,18 @@ public class UserThread extends Thread{
     private final Gate gate;
     private final String myName;
     private final String myAddress;
+    private final int count;
 
-    public UserThread(Gate gate, String myName, String myAddress) {
+    public UserThread(Gate gate, String myName, String myAddress, int count) {
         this.gate = gate;
         this.myName = myName;
         this.myAddress = myAddress;
+        this.count = count;
     }
 
     public void run() {
         System.out.println(myName + " BEGIN");
-        for(int i = 0; i < 100000; i++) {
+        for(int i = 0; i < count; i++) {
             gate.pass(myName, myAddress);
         }
     }
