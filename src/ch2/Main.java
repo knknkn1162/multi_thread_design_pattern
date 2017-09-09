b857d51829f5b79a57f899b3183eb27c8c28e843
knknkn1162 <knknkn1162@gmail.com>
2017-09-09 17:21:00 +0900

    add Main class

--- src/ch2/Main.java
+++ src/ch2/Main.java
@@ -4,4 +4,16 @@ package ch2;
  * Created by kenta_nakajima on 2017/09/09.
  */
 public class Main {
+    public static void main(String[] args) {
+        System.out.println("Testing Gate");
+        Gate gate = new Gate();
+        String[][] info = {
+                {"Alice", "Alaska"},
+                {"Bobby", "Brazil"},
+                {"Chris", "Canada"},
+        };
+        for (String[] s: info) {
+            new UserThread(gate, s[0], s[1]).start();
+        }
+    }
 }
