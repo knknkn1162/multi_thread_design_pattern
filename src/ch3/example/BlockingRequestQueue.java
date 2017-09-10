8e72b08f17b63fd8a9c46083668f7b47d400ff3e
knknkn1162 <knknkn1162@gmail.com>
2017-09-10 16:01:15 +0900

    impl BlockingRequestQueue class

--- src/ch3/example/BlockingRequestQueue.java
+++ src/ch3/example/BlockingRequestQueue.java
@@ -3,5 +3,25 @@ package ch3.example;
 /**
  * Created by knknkn on 2017/09/10.
  */
+
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.LinkedBlockingQueue;
+
 public class BlockingRequestQueue {
+    private final BlockingQueue<Request> queue = new LinkedBlockingQueue<Request>();
+
+    public Request getRequest() {
+        Request req = null;
+        try {
+            req = queue.take();
+        } catch (InterruptedException e) {}
+        return req;
+    }
+
+    public void putRequest(Request request) {
+        try {
+            queue.put(request);
+
+        } catch (InterruptedException e) {}
+    }
 }
