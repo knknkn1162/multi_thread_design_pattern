c33459a4c60efe55885d572cf0a3936833cc72de
knknkn1162 <knknkn1162@gmail.com>
2017-09-10 16:40:54 +0900

    set thread sleep

--- src/ch3/exercise5/TalkThread.java
+++ src/ch3/exercise5/TalkThread.java
@@ -17,12 +17,17 @@ public class TalkThread extends Thread {
     public void run() {
         System.out.println(Thread.currentThread().getName() + ":BEGIN");
         for (int i = 0; i < 20; i++) {
+
             Request request1 = input.getRequest();
             System.out.println(Thread.currentThread().getName() + " gets " + request1);
 
             Request request2 = new Request(request1.getName() + "!");
             System.out.println(Thread.currentThread().getName() + " puts " + request2);
             output.putRequest(request2);
+
+            try {
+                Thread.sleep(100);
+            } catch (InterruptedException e) {}
         }
 
         System.out.println(Thread.currentThread().getName() + ":END");
