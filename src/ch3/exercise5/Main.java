87d519463bb1233d2fee230127c3e95a0f0c2847
knknkn1162 <knknkn1162@gmail.com>
2017-09-10 16:40:27 +0900

    set trigger in main method

--- src/ch3/exercise5/Main.java
+++ src/ch3/exercise5/Main.java
@@ -7,7 +7,8 @@ public class Main {
     public static void main(String[] args) {
         RequestQueue requestQueue1 = new RequestQueue();
         RequestQueue requestQueue2 = new RequestQueue();
-
+        //set trigger
+        requestQueue1.putRequest(new Request("Hello"));
         new TalkThread(requestQueue1, requestQueue2, "Alice").start();
         new TalkThread(requestQueue2, requestQueue1, "Bobby").start();
     }
