de5681aba9853729badc68160a92957510e0cf5c
knknkn1162 <knknkn1162@gmail.com>
2017-09-10 17:31:40 +0900

    impl ch4.option1

---
+++ src/ch4/option1/Main.java
@@ -0,0 +1,21 @@
+package ch4.option1;
+
+/**
+ * Created by knknkn on 2017/09/10.
+ */
+
+import java.util.concurrent.TimeoutException;
+
+public class Main {
+    public static void main(String[] args) {
+        Host host = new Host(10000);
+        try {
+            System.out.println("execute Begin");
+            host.execute();
+        } catch (TimeoutException e) {
+            e.printStackTrace();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
