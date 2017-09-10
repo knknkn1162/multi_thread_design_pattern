cc3cc4a191707e0eff5909aab32c2b3292bdae63
knknkn1162 <knknkn1162@gmail.com>
2017-09-10 10:51:24 +0900

    make eat method synchronized

--- src/ch1/exercise6/EaterThread.java
+++ src/ch1/exercise6/EaterThread.java
@@ -20,15 +20,12 @@ public class EaterThread extends Thread{
         }
     }
 
-    public void eat() {
-        synchronized (lefthand) {
-            System.out.println(name + "takes up " + lefthand + " (left).");
-            synchronized (righthand) {
-                System.out.println(name + " takes up " + righthand + " (right).");
-                System.out.println(name + " is eating now, yum yum!");
-                System.out.println(name + " puts down " + righthand + "  (right).");
-            }
-            System.out.println(name + " puts down " + lefthand + " (left).");
-        }
+    public synchronized void eat() {
+        System.out.println(name + "takes up " + lefthand + " (left).");
+        System.out.println(name + " takes up " + righthand + " (right).");
+        System.out.println(name + " is eating now, yum yum!");
+        System.out.println(name + " puts down " + righthand + "  (right).");
+        System.out.println(name + " puts down " + lefthand + " (left).");
     }
+
 }
