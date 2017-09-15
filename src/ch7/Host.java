7317a8d940f0376ec307609478e84be82bd7715a
knknkn1162 <knknkn1162@gmail.com>
2017-09-15 16:50:08 +0900

    imple ch7

---
+++ src/ch7/Host.java
@@ -0,0 +1,19 @@
+package ch7;
+
+/**
+ * Created by kenta_nakajima on 2017/09/15.
+ */
+public class Host {
+    private final Helper helper = new Helper();
+    public void request(final int count, final char c) {
+        System.out.println("request(" + count + ", " + c + ") begin");
+
+        new Thread() {
+            public void run() {
+                helper.handle(count, c);
+            }
+        }.start();
+
+        System.out.println("request(" + count + ", " + c + ") end");
+    }
+}
