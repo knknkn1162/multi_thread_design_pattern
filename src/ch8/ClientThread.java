7d8dd75972c0b4a6127d029da4fdf47118f59127
knknkn1162 <knknkn1162@gmail.com>
2017-09-16 14:43:05 +0900

    impl ClientThread & Main

--- src/ch8/ClientThread.java
+++ src/ch8/ClientThread.java
@@ -3,5 +3,25 @@ package ch8;
 /**
  * Created by knknkn on 2017/09/16.
  */
-public class ClientThread {
+
+import java.util.Random;
+
+public class ClientThread extends Thread {
+    private final Channel channel;
+    private static final Random random = new Random();
+
+    public ClientThread(String name, Channel channel) {
+        super(name);
+        this.channel = channel;
+    }
+
+    public void run() {
+        try {
+            for(int i =0; true; i++) {
+                Request request = new Request(getName(), i);
+                channel.putRequest(request);
+                Thread.sleep(random.nextInt(1000));
+            }
+        } catch (InterruptedException e) {}
+    }
 }
---
+++ src/ch8/Main.java
@@ -0,0 +1,15 @@
+package ch8;
+
+/**
+ * Created by knknkn on 2017/09/16.
+ */
+public class Main {
+    public static void main(String[] args) {
+        Channel channel = new Channel(5);
+        channel.startWorkers();
+
+        new ClientThread("Alice", channel).start();
+        new ClientThread("Bob", channel).start();
+        new ClientThread("Chris", channel).start();
+    }
+}
