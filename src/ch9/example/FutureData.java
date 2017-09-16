3f6ad593c3c0492d85b4476e0a1f77cb233e2a00
knknkn1162 <knknkn1162@gmail.com>
2017-09-16 16:03:41 +0900

    impl FutureDat

---
+++ src/ch9/example/FutureData.java
@@ -0,0 +1,28 @@
+package ch9.example;
+
+/**
+ * Created by knknkn on 2017/09/16.
+ */
+public class FutureData implements Data {
+    private RealData realdata = null;
+    private boolean ready = false;
+
+    public synchronized void setRealdata(RealData realdata) {
+        if (ready) { return;}
+        this.realdata = realdata;
+        this.ready = true;
+        notifyAll();
+    }
+
+    public synchronized String getContent() {
+        while(ready) {
+            try {
+                wait();
+            } catch (InterruptedException e) {}
+        }
+
+        return realdata.getContent();
+    }
+
+
+}
