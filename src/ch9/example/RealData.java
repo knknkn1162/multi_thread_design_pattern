29b0df4846c0adc488c9434aba1567e0dee43f44
knknkn1162 <knknkn1162@gmail.com>
2017-09-16 15:09:12 +0900

    impl RealData

---
+++ src/ch9/example/RealData.java
@@ -0,0 +1,25 @@
+package ch9.example;
+
+/**
+ * Created by knknkn on 2017/09/16.
+ */
+public class RealData implements Data {
+    private final String content;
+    public RealData(int count, char ch) {
+        System.out.println("maken RealData(" + count + ", " + ch + ") begin");
+
+        char[] buffer = new char[count];
+
+        for(int i = 0; i < count; i++) {
+            buffer[i] = ch;
+            try { Thread.sleep(100);} catch (InterruptedException e) {}
+        }
+
+        System.out.println("making RealData(" + count + ", " + ch + ") end");
+        this.content = new String(buffer);
+     }
+
+     public String getContent() {
+        return content;
+     }
+}
