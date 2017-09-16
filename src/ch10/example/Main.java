71c6140b9cbfb506248dd0ad6b709c37a6649eba
knknkn1162 <knknkn1162@gmail.com>
2017-09-16 17:44:08 +0900

    impl src/ch10

---
+++ src/ch10/example/Main.java
@@ -0,0 +1,28 @@
+package ch10.example;
+
+/**
+ * Created by knknkn on 2017/09/16.
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("main: BEGIN");
+
+        try {
+            CountupThread t = new CountupThread();
+            t.start();
+
+            Thread.sleep(10000);
+
+            System.out.println("main: shutdownRequest");
+            t.shutdownRequest();
+
+            System.out.println("main: join");
+
+            t.join();
+
+        } catch (InterruptedException e) {}
+
+        System.out.println("main: END");
+    }
+
+}
