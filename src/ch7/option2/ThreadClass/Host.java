0512f567526fc3b68144955ae91ba4f4266c52c0
knknkn1162 <knknkn1162@gmail.com>
2017-09-18 14:27:38 +0900

    impl ch7.option2

---
+++ src/ch7/option2/ThreadClass/Host.java
@@ -0,0 +1,19 @@
+package ch7.option2.ThreadClass;
+
+/**
+ * Created by kenta_nakajima on 2017/09/18.
+ */
+public class Host {
+    private final Helper helper = new Helper();
+
+    public void request(final int count, final char ch) {
+        System.out.println("request( " + count + ", " + ch + ") begin");
+        new Thread() {
+            public void run() {
+                helper.handle(count, ch);
+            }
+        }.start();
+
+        System.out.println("request( " + count + ", " + ch + ") end");
+    }
+}
