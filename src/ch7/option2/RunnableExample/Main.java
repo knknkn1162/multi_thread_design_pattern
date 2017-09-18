2a71ed67bd7048cc67cfc28bc96e382106e16806
knknkn1162 <knknkn1162@gmail.com>
2017-09-18 14:32:54 +0900

    imple Runnable Interface Example

---
+++ src/ch7/option2/RunnableExample/Main.java
@@ -0,0 +1,19 @@
+package ch7.option2.RunnableExample;
+
+import ch7.example.Host;
+
+/**
+ * Created by kenta_nakajima on 2017/09/15.
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("main begin");
+
+        Host host = new Host();
+        host.request(10, 'A');
+        host.request(20, 'B');
+        host.request(30, 'c');
+
+        System.out.println("main END");
+    }
+}
