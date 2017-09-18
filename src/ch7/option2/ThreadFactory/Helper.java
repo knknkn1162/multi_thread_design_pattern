efc35e709ac4d6017a4acee95dd7fc71cbb0fe57
knknkn1162 <knknkn1162@gmail.com>
2017-09-18 14:44:41 +0900

    impl ch7.option2 using ThreadFactory

---
+++ src/ch7/option2/ThreadFactory/Helper.java
@@ -0,0 +1,23 @@
+package ch7.option2.ThreadFactory;
+
+/**
+ * Created by kenta_nakajima on 2017/09/15.
+ */
+public class Helper {
+    public void handle(int count, char c) {
+        System.out.println(" handle(" + count + ", " + c + ") begin");
+
+        for(int i = 0 ; i < count; i++) {
+            slowly();
+            System.out.print(c);
+        }
+        System.out.println("");
+        System.out.println(" handle(" + count + "," + c + ") end");
+    }
+
+    private void slowly() {
+        try {
+            Thread.sleep(100);
+        } catch (InterruptedException e) {}
+    }
+}
---
+++ src/ch7/option2/ThreadFactory/Host.java
@@ -0,0 +1,31 @@
+package ch7.option2.ThreadFactory;
+
+/**
+ * Created by kenta_nakajima on 2017/09/18.
+ */
+
+import java.util.concurrent.ThreadFactory;
+
+public class Host {
+    private final Helper helper = new Helper();
+    private final ThreadFactory threadFactory;
+
+    public Host(ThreadFactory threadFactory) {
+        this.threadFactory = threadFactory;
+    }
+
+    public void request(final int count, final char ch) {
+        System.out.println("request(" + count + ", " + ch + ") begin");
+
+        threadFactory.newThread(
+                new Runnable() {
+                    @Override
+                    public void run() {
+                        helper.handle(count, ch);
+                    }
+                }
+        ).start();
+
+        System.out.println("request( " + count + ", " + ch + ") end");
+    }
+}
